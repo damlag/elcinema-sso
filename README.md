bb749b77692e3bc90c36c6dbb03c52e7ccc24357
Mustafa Zain <mzain@mzain.damlag.lan>
2015-03-26 14:53:44 +0200

    - Elcinema sso plugin

---
+++ README.md
@@ -0,0 +1,24 @@
+### elcinema-sso
+
+This plugin provides remotely sso login functionality to Discourse. When users login
+the main website.
+
+## Installation
+
+Follow our [Install a Plugin](https://meta.discourse.org/t/install-a-plugin/19157) howto, using
+`git clone https://github.com/MZainDamlag/elcinema-sso.git` as the plugin command.
+
+Once you've installed it, review the settings under admin and then enable
+`remote_sso_enabled`.
+
+## Features
+
+- Users can login remotely to forum without needing to visit or be redirected to it when he login in the main website
+
+## Issues
+
+If you have issues or suggestions for the plugin, please bring them up on [Discourse Meta](https://meta.discourse.org).
+
+## License
+
+MIT
---
+++ plugin.rb
@@ -0,0 +1,75 @@
+# name: elcinema-sso
+# about: Support for remote sso in Discourse
+# version: 0.1
+# authors: 'Mustafa Zain Eddin'
+# url: https://github.com/MZainDamlag/elcinema-sso
+
+enabled_site_setting :remote_sso_enabled
+
+after_initialize do
+
+  Discourse::Application.routes.append do
+    get "session/elcinema_sso" => "session#elcinema_sso"
+    get "session/elcinema_sso_login" => "session#elcinema_sso_login"    
+  end
+
+  ::SessionController.skip_before_filter :check_xhr, only: ['sso', 'elcinema_sso', 'sso_login', 'elcinema_sso_login', 'become', 'sso_provider']
+
+  add_to_class(:session_controller, :elcinema_sso) do
+    if SiteSetting.enable_sso
+      @url = DiscourseSingleSignOn.generate_url(params[:return_path])
+      render :json => data = { url: @url }, :status => '200'
+    else
+      render nothing: true, status: 404
+    end
+  end
+
+  add_to_class(:session_controller, :elcinema_sso_login) do
+    unless SiteSetting.enable_sso
+      render nothing: true, status: 404
+      return
+    end
+
+    sso = DiscourseSingleSignOn.parse(request.query_string)
+    if !sso.nonce_valid?
+      render text: I18n.t("sso.timeout_expired"), status: 500
+      return
+    end
+
+    return_path = sso.return_path
+    sso.expire_nonce!
+
+    begin
+      if user = sso.lookup_or_create_user
+        if SiteSetting.must_approve_users? && !user.approved?
+          render text: I18n.t("sso.account_not_approved"), status: 403
+        else
+          log_on_user user
+        end
+
+        # If it's not a relative URL check the host
+        # if return_path !~ /^\/[^\/]/
+        #   begin
+        #     uri = URI(return_path)
+        #     return_path = "/" unless uri.host == Discourse.current_hostname
+        #   rescue
+        #     return_path = "/"
+        #   end
+        # end
+
+        render :json => user , :status => '200'
+        # redirect_to return_path
+      else
+        render text: I18n.t("sso.not_found"), status: 500
+      end
+    rescue => e
+      details = {}
+      SingleSignOn::ACCESSORS.each do |a|
+        details[a] = sso.send(a)
+      end
+      Discourse.handle_exception(e, details)
+
+      render text: I18n.t("sso.unknown_error"), status: 500
+    end
+  end
+end
