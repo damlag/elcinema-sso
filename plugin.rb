9556e7339d7ece450f9bc731d794d7c6bb6151e2
Mustafa Zain Eddin <mustafa.m.zeineddin@ieee.org>
2016-07-03 17:54:13 +0200

    Fix typo

--- plugin.rb
+++ plugin.rb
@@ -11,7 +11,7 @@ after_initialize do
   Discourse::Application.routes.append do
     get "session/elcinema_sso" => "session#elcinema_sso"
     get "session/elcinema_sso_login" => "session#elcinema_sso_login"
-    delete "sessions/:id" => "session#elcinema_sso_logout", constraints:  {:id=>/[\w.\-]+/} if SiteSetting.enable_sso
+    delete "session/:id" => "session#elcinema_sso_logout", constraints:  {:id=>/[\w.\-]+/} if SiteSetting.enable_sso
   end
 
   ::SessionController.skip_before_filter :check_xhr, only: ['sso', 'elcinema_sso', 'sso_login', 'elcinema_sso_login', 'become', 'sso_provider']
